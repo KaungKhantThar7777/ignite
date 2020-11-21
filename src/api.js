7842f2ea41dc50d607548bdfe57d6c6e795020a1
kaung khant thar <kaungkhantthar@ucsm.edu.mm>
2020-11-21 12:29:38 +0630

    Initial Setup

---
+++ src/api.js
@@ -0,0 +1,28 @@
+//Base URL
+const base_url = "https://api.rawg.io/api/";
+
+//Getting the date
+const getCurrentMonth = () => {
+  const month = new Date().getMonth() + 1;
+  return `${month}`.padStart(2, "0");
+};
+//Getting the date
+const getCurrentDay = () => {
+  const day = new Date().getDate();
+  return `${day}`.padStart(2, "0");
+};
+
+//Current year/month/day
+const currentYear = new Date().getFullYear();
+const currentMonth = getCurrentMonth();
+const currentDay = getCurrentDay();
+const currentDate = `${currentYear}-${currentMonth}-${currentDay}`;
+const lastYear = `${currentYear - 1}-${currentMonth}-${currentDay}`;
+const nextYear = `${currentYear + 1}-${currentMonth}-${currentDay}`;
+
+//Popular Games
+const popular_games = `games?dates=${lastYear},${currentDate}&ordering=-rating&page_size=10`;
+const upcoming_games = `games?dates=${currentDate},${nextYear}&ordering=-added&page_size=10`;
+const newGames = `games?dates=${lastYear},${currentDate}&ordering=-released&page_size=10`;
+
+export const popularGamesURL = () => `${base_url}${popular_games}`;
